ccd5d90915e15d9ace7a0c61172679684e95514e
Dejan <38137502+Dejan1990@users.noreply.github.com>
2022-01-13 15:42:06 +0100

    Attributes Section part-1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Database\Seeders\SettingSeeder;
 use Database\Seeders\CategorySeeder;
+use Database\Seeders\AttributesTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -28,9 +29,12 @@ class DatabaseSeeder extends Seeder
             'email' => 'lucy@mail.com'
         ]);
 
+        //$this->call(AdminsTableSeeder::class); ovo mogu da napravim za admina umesto ovo iznad
+
         $this->call([
             SettingSeeder::class,
-            CategorySeeder::class
+            CategorySeeder::class,
+            AttributesTableSeeder::class
         ]);
     }
 }
